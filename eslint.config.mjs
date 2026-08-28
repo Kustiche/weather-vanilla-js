58e8cb28568a0b13664abd6319fc7f5f24d425b8
Kustiche <ChernichnyKustik@mail.ru>
2026-08-28 17:35:45 +0300

    🔧 chore(eslint\prettier): устранил конфликты между eslint и prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,16 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import { defineConfig } from 'eslint/config';
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
-  {
-    files: ['./src/js/**/*.js'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser },
-    rules: {
-      'no-console': 'warn',
-    },
-  },
-]);
+	{
+		files: ['./src/js/**/*.js'],
+		plugins: { js },
+		extends: ['js/recommended', eslintConfigPrettier],
+		languageOptions: { globals: globals.browser },
+		rules: {
+			'no-console': 'warn',
+		},
+	},
+])
